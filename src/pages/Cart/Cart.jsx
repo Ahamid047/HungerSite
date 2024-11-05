565eb09a376698ba68d99f3742dbd1bb0d0456ba
Abdul Hamid <ahamid9044@gmail.com>
2024-11-05 12:38:39 +0530

    created place order page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react'
 import "./Cart.css"
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 const Cart = () => {
 
   const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
+
+  const navigate=useNavigate();
   return (
     <>
     <div className='cart'>
@@ -50,15 +53,15 @@ const Cart = () => {
           <hr />
           <div className="cart-total-details">
             <p>Delivery Fee</p>
-            <p>₹{30}</p>
+            <p>₹{getTotalCartAmount()===0?0:30}</p>
           </div>
           <hr />
           <div className="cart-total-details">
             <p>Total</p>
-            <p>₹{getTotalCartAmount()+30}</p>
+            <p>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+30}</p>
           </div>
         </div>
-        <button>PROCEED TO CHECKOUT</button>
+        <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
 
       </div>
       <div className="cart-promocode">
