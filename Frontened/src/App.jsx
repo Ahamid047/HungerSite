3b30951658c22a40f5b992ac17915116b96baeaf
Abdul Hamid <ahamid9044@gmail.com>
2024-11-26 21:48:14 +0530

    adding the payment functionality

--- Frontened/src/App.jsx
+++ Frontened/src/App.jsx
@@ -6,6 +6,7 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+import Verify from './pages/Verify/Verify'
 
 const app = () => {
   const [showLogin, setShowLogin]=useState(false)
@@ -19,6 +20,8 @@ const app = () => {
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/Verify' element={<Verify />} />
+
         </Routes>
       </div>
       <Footer />
